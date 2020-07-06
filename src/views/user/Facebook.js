b57d75a633e7c38d9043764c680bc6ec36560bf2
Kamynale <alexandrkamynin@gmail.com>
2020-07-06 13:59:43 +0300

    Получил данные пользователя в Frontend

--- src/views/user/Facebook.js
+++ src/views/user/Facebook.js
@@ -9,28 +9,35 @@ export default class Facebook extends Component {
         auth: false,
         name: '',
         email: '',
-        picture: ''
+        picture: '',
+        token: ''
     };
 
         responseFacebook = response => {
-            if(response && response.status !== 'unknown' && response.picture) {
+            if(response && response.status !== 'unknown' && response.picture)
+                {
                 this.props.onUserLogin({
                     ...response,
                     email: 'demo@gogo.com',
                     password: 'gogo123',
                 });
 
-                let facebookName = response.name;
-                let facebookEmail = response.email;
-                let facebookPicture = response.picture;
-                console.log(facebookName, facebookEmail,facebookPicture);
+                // Токен пользователя
+                let token = (response.accessToken)
+                console.log(token)
 
-    
+                // Get запрос данных пользователя с токеном
+                FB.api('/me', 'get', { access_token: token, fields: 'id,name,gender,email' }, function(response) {
+                    console.log(response);
+                    let user = response;
+                  });
+                
                 this.setState({
                     auth: true,
                     name: response.name,
                     email: response.email,
-                    picture: response.picture.data.url
+                    picture: response.picture.data.url,
+                    token: response.accessToken
                 });
             }
         }
@@ -45,34 +52,10 @@ export default class Facebook extends Component {
                     autoLoad={true}
                     fields="name,email,picture"
                     onClick={this.componentClicked}
-                    callback={this.responseFacebook} />);
+                    callback={this.responseFacebook} 
+                    />);
             }
 
-            FB.api('oauth/access_token',  { 
-                client_id: '2908344615961117', 
-                client_secret: '13f9f790ee0de527a44ac561c4786b7c', 
-                grant_type:'client_credentials'
-            },  function (res)  { 
-                if( !res ||  res.error)  { 
-                    console.log( !res? 'error occurred': res.error); 
-                    return;
-                } 
-            
-                var accessToken =  res.access_token;
-                var expires = res.expires ? res.expires : 0;
-                console.log(accessToken)
-
-                FB.api('',  function (res)  { 
-                    if( !res ||  res.error)  { 
-                     console.log( !res? 'error occurred': res.error); 
-                     return;
-                    } 
-                    console.log( res.id); 
-                    console.log( res.name);
-                  } );
-
-            } );
-            
             
             return (
                 <>
