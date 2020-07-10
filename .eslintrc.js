ff3923605dcaf72ec57fe70822000059645ea9cc
psk <pskucherov@yandex.ru>
2020-07-11 00:29:08 +0300

    eslint start

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,33 +6,37 @@ module.exports = {
     },
     "parser": "babel-eslint",
     "extends": [
-      "react-app",
-      "prettier",
-      "airbnb",
-      "plugin:import/errors",
-      "plugin:import/warnings",
-      "plugin:jsx-a11y/recommended",
-      "plugin:sonarjs/recommended",
-      "plugin:promise/recommended"
+        "react-app",
+        "prettier",
+        "airbnb",
+        "plugin:import/errors",
+        "plugin:import/warnings",
+        "plugin:jsx-a11y/recommended",
+        "plugin:sonarjs/recommended",
+        "plugin:promise/recommended",
     ],
     "plugins": ["react",  "jsx-a11y", "optimize-regex", "sonarjs", "no-loops", "no-use-extend-native", "promise"],
-  "rules": {
-    "jsx-quotes": [
-        1,
-        "prefer-double"
-      ],
-    "react/jsx-filename-extension": "off",
-    "jsx-a11y/click-events-have-key-events": "off",
-    "import/no-extraneous-dependencies": [
-      "error",
-      {
-        "packageDir": "./"
-      }
-    ],
-    "allowTernary": 1,
-    "optimize-regex/optimize-regex": "warn",
-    "sonarjs/cognitive-complexity": ["error", 30],
-    "no-loops/no-loops": 2,
-    "no-use-extend-native/no-use-extend-native": 2,
-  }
+    "rules": {
+        "template-curly-spacing" : "off",
+        "indent": ["error", 4, {
+            "ignoredNodes": ["TemplateLiteral"]
+        }],
+        "jsx-quotes": [
+            1,
+            "prefer-double"
+        ],
+        "react/jsx-filename-extension": "off",
+        "jsx-a11y/click-events-have-key-events": "off",
+        "import/no-extraneous-dependencies": [
+            "error",
+            {
+                "packageDir": "./"
+            }
+        ],
+        "allowTernary": 1,
+        "optimize-regex/optimize-regex": "warn",
+        "sonarjs/cognitive-complexity": ["error", 30],
+        "no-loops/no-loops": 2,
+        "no-use-extend-native/no-use-extend-native": 2,
+    }
 };
