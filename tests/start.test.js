eaf4cd598ee4f65d6e2049d54a634ef06586194b
Kamynale <alexandrkamynin@gmail.com>
2020-08-07 20:59:58 +0300

    enzyme

--- tests/start.test.js
+++ tests/start.test.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Start from '../src/views/app/gogo/start';
+import {shallow} from 'enzyme';
 
 
 describe("Start", () => {
     it("Should render chart", () => {
-        const component = renderer.create(<Start/>);
+        const component = shallow(<Start/>);
         expect(component).toMatchSnapshot();
     })
 });
