3808a8d550e24b956f8b9eca813f771bd1a0bc45
Kamynale <alexandrkamynin@gmail.com>
2020-08-31 23:51:32 +0300

    Transaction

--- server/transaction.js
+++ server/transaction.js
@@ -10,7 +10,6 @@ const connection = mysql.createConnection(
         database: 'opexbetadb',
     },
 );
-
 connection.connect(err => {
     if (err) {
         console.error(`error connecting: ${err.stack}`);
@@ -19,51 +18,55 @@ connection.connect(err => {
     console.log(`connected as id ${connection.threadId}`);
 });
 
-/* // Create values
-const name = 'INSERT INTO Transactions(id,balance,name,price) VALUES('1','10000','Alexandr Kamynin','1000');
-const balance = 'VALUES(1, 10000)';
+const sql = 'SELECT balance FROM Transactions  WHERE id=2 AND balance>=1000';
 
-connection.query(name, (err, results) => {
-    if (err) console.log(err);
-    console.log(results);
-}); */
+connection.query(sql, (err, results) => {
+    if (err) throw err;
+    if (results && results.length === 0) {
+        console.log('Not enough money');
+    } else {
+        console.log(results);
 
-/* Begin transaction */
-connection.beginTransaction(err => {
-    if (err) { throw err }
-
-    connection.query({
-        sql: 'UPDATE Transactions Set balance = balance - ? where ?',
-        values: [1000, { id: 1 }],
-    },
-    (error, results, fields) => {
-        if (error) {
-            return connection.rollback(() => {
-                throw error;
-            });
-        }
-
-        const log = `Transactions ${ results.insertId } added`;
-
-        connection.query({
-            sql: 'UPDATE Transactions Set balance = balance + ? where ?',
-            values: [1000, { id: 2 }],
-        },
-        (error, results, fields) => {
-            if (error) {
-                return connection.rollback(() => {
-                    throw error;
-                });
+        /* Begin transaction */
+        connection.beginTransaction(err => {
+            if (err) {
+                throw err;
             }
-            connection.commit(err => {
-                if (err) {
+
+            connection.query({
+                sql: 'UPDATE Transactions Set balance = balance - ? where ?',
+                values: [1000, { id: 1 }],
+            },
+            (error, results, fields) => {
+                if (error) {
                     return connection.rollback(() => {
-                        throw err;
+                        throw error;
                     });
                 }
-                console.log('success!');
+
+                const log = `Transactions ${results.insertId} added`;
+
+                connection.query({
+                    sql: 'UPDATE Transactions Set balance = balance + ? where ?',
+                    values: [1000, { id: 2 }],
+                },
+                (error, results, fields) => {
+                    if (error) {
+                        return connection.rollback(() => {
+                            throw error;
+                        });
+                    }
+                    connection.commit(err => {
+                        if (err) {
+                            return connection.rollback(() => {
+                                throw err;
+                            });
+                        }
+                        console.log('success!');
+                    });
+                });
             });
         });
-    });
+        /* End transaction */
+    }
 });
-/* End transaction */
