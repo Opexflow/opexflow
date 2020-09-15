f8cb5dbd1dd20a54c6fcf7545b98801c76e922b3
Julia <tarasova.bmstu@gmail.com>
2020-09-15 13:42:28 +0300

    bugs fixed

--- server/config.js
+++ server/config.js
@@ -1,8 +1,6 @@
 module.exports = {
-    // facebook_api_key: '2969806906408505',
-    // facebook_api_secret: '4e4c8607d69bec65298a406838026b89',
-    facebook_api_key: '3606748869337149',
-    facebook_api_secret: '28d6d64b25d647d30f08f3e9c489eb04',
+    facebook_api_key: '2969806906408505',
+    facebook_api_secret: '4e4c8607d69bec65298a406838026b89',
     // 'http://localhost:3001/api/auth/facebook/callback',
     callback_url: 'https://opexflow.com/api/auth/facebook/callback',
     host: '78.24.216.16',
