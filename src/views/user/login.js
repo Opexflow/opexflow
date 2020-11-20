53116e2d0376cf622a8aedef5119f581ca1f98cf
Egor <bev1488@ya.ru>
2020-11-20 13:10:38 +0400

    egor20.11

--- src/views/user/login.js
+++ src/views/user/login.js
@@ -81,7 +81,8 @@ class Login extends Component {
               <Colxx xxs="12" md="10" className="mx-auto my-auto">
                   <Card className="auth-card">
                       <div className="position-relative image-side ">
-                          <p className="text-white h2">MAGIC IS IN THE DETAILS</p>
+                          <p className="lead text-white">
+          <IntlMessages id="dashboards.magic-is-in-the-details" /> </p>
                           <p className="white mb-0">
                           Please use your credentials to login.
                               <br />
