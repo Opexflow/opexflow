5902e46fb10e2aa7f1734674bd03ae3e76450554
Egor <bev1488@ya.ru>
2020-11-28 17:33:59 +0400

    Revert "Revert " multinational lang""
    This reverts commit 0335d93862583e119bf0ca6c788e5abdf3a31532.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import {
 import { IntlProvider } from 'react-intl';
 // import './helpers/Firebase';
 import AppLocale from './lang';
+import ruLang from './lang/entries/ru-RU';
+
 // import ColorSwitcher from './components/common/ColorSwitcher';
 import NotificationContainer from './components/common/react-notifications/NotificationContainer';
 import { getDirection } from './helpers/Utils';
@@ -50,14 +52,21 @@ class App extends Component {
 
     render() {
         const { locale, loginUser } = this.props;
-        const currentAppLocale = AppLocale[locale];
+        var currentAppLocale = AppLocale[locale];
 
+  
+        var lang = navigator.browserLanguage || navigator.language || navigator.userLanguage;    
+        if (lang.substring(0, 2) == 'ru')  {
+                currentAppLocale.locale='ru-RU';
+                currentAppLocale.messages=ruLang.messages;
+          }
+    
         return (
             <div className="h-100">
-                <IntlProvider
-                    locale={currentAppLocale.locale}
-                    messages={currentAppLocale.messages}
-              >
+             <IntlProvider
+locale={currentAppLocale.locale}
+messages={currentAppLocale.messages}
+>
                     <>
                     <NotificationContainer />
                     { /* isMultiColorActive && <ColorSwitcher /> */ }
