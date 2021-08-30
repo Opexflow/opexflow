389539fd818fd1dfb4a038453ecd1b45627f16c4
prashansa-shukla <prashufreelance@gmail.com>
2021-08-30 21:48:57 +0530

    commenting out notused routes/pages

--- src/containers/navs/Topnav.EasyAccess.js
+++ src/containers/navs/Topnav.EasyAccess.js
@@ -14,7 +14,7 @@ const TopnavEasyAccess = () => (
               right
               id="iconMenuDropdown"
             >
-              <NavLink to="/app/dashboards/default" className="icon-menu-item">
+            {/*<NavLink to="/app/dashboards/default" className="icon-menu-item">
                   <i className="iconsminds-shop-4 d-block" />
                     {' '}
                   <IntlMessages id="menu.dashboards" />
@@ -25,7 +25,7 @@ const TopnavEasyAccess = () => (
                     {' '}
                   <IntlMessages id="menu.ui" />
                 </NavLink>
-              {/* <NavLink to="/app/ui/charts" className="icon-menu-item">
+              <NavLink to="/app/ui/charts" className="icon-menu-item">
                   <i className="iconsminds-bar-chart-4 d-block" />
                     {' '}
                   <IntlMessages id="menu.charts" />
@@ -35,7 +35,7 @@ const TopnavEasyAccess = () => (
                     {' '}
                   <IntlMessages id="menu.chat" />
                 </NavLink>
-              <NavLink to="/app/applications/survey" className="icon-menu-item">
+              {/* <NavLink to="/app/applications/survey" className="icon-menu-item">
                   <i className="iconsminds-formula d-block" />
                     {' '}
                   <IntlMessages id="menu.survey" />
@@ -44,7 +44,7 @@ const TopnavEasyAccess = () => (
                   <i className="iconsminds-check d-block" />
                     {' '}
                   <IntlMessages id="menu.todo" />
-                </NavLink>
+                </NavLink> */}
               <NavLink to="/app/applications/marketplace" className="icon-menu-item">
                   <i className="iconsminds-factory d-block" />
                     {' '}
